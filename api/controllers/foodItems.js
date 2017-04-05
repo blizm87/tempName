088e8adf56dbae2e74c4fef364fe6b0bb9c4d6a0
Justin Samuels <blizm87@gmail.com>
2017-04-04 20:32:57 -0700

    building route functionality

--- api/controllers/foodItems.js
+++ api/controllers/foodItems.js
@@ -1,5 +1,13 @@
-function getAll(request, response) {
+const request = require('request');
 
+function getAll(req, resp) {
+  let url = 'http://api.brewerydb.com/v2/breweries';
+  let access_token = '154b55a2e97a7540aa3b961e81a7323f';
+  request.get(url, (err, resp, body) => {
+    console.log(body);
+    console.log(resp);
+    console.log(err);
+  })
 }
 
 module.exports = {
--- public/controllers/foodItemsController.js
+++ public/controllers/foodItemsController.js
@@ -2,13 +2,21 @@
   'use strict';
 
   angular
-    .module('tempNameApp')
+    .module('tempNameApp', [])
     .controller('foodItemsController', foodItemsController);
 
     foodItemsController.$inject = ['$http'];
-    function foodItemsController(){
+    function foodItemsController($http){
       let vm = this;
       vm.all = [];
+      $http
+        .get('/foodItems')
+        .then(function(response){
+          vm.data = response;
+          console.log(vm.data)
+        }, function(err){
+          console.log(err)
+        });
 
     }
 })();
